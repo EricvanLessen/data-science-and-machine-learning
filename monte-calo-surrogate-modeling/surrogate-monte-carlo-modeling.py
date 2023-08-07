8f209a77226332846483793dafab92d7fc9a8edc
EricvanLessen <eric.vanlessen@googlemail.com>
2023-08-07 14:27:46 +0200

    surrogate modeling mcmc neural network

--- monte-calo-surrogate-modeling/surrogate-monte-carlo-modeling.py
+++ monte-calo-surrogate-modeling/surrogate-monte-carlo-modeling.py
@@ -1,45 +1,57 @@
-# pip install pymc3 pandas tensorflow
+# todo: write modular code
 
+import logging
+import os
 
-import pandas as pd
-import pymc3 as pm
+import torch
 import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import pyro
+
+import pyro.distributions as dist
+
+from pyro.infer import MCMC, NUTS
+
 import tensorflow as tf
 
-# 1. Load Data
-df = pd.read_csv('Binance_1INCHBTC_d.csv')
-data = df['Close'].values
+# 1.0. Load Data
+df = pd.read_csv('Binance_1INCHBTC_d.csv', skiprows=1)
+df.head()
 
-# 2. Prepare Sequences
-def create_sequences(data, window_size=10):
-    X, y = [], []
-    for i in range(len(data) - window_size):
-        X.append(data[i:i+window_size-1])
-        y.append(data[i+window_size-1])
-    return np.array(X), np.array(y)
+# 1.1. Load the data 
+close_prices = df['Close'].values
+close_prices[0:10]
 
-X, y = create_sequences(data)
+# 2. Prepare sequences
+window_size = 10
+sequences = [close_prices[i: i + window_size] for i in range(len(close_prices) - window_size + 1)]
 
-# 3. Set Up a Bayesian Model in PyMC3 and Sampling
-with pm.Model() as model:
-    # Priors
-    sigma = pm.HalfCauchy('sigma', beta=10, testval=1.)
-    intercept = pm.Normal('Intercept', 0, sd=20)
-    x_coeff = pm.Normal('x_coeff', 0, sd=20, shape=9)
-    
-    # Expected outcome
-    mu = intercept + pm.math.dot(X, x_coeff)
-    
-    # Likelihood
-    price_obs = pm.Normal('price_obs', mu=mu, sd=sigma, observed=y)
+# 3. Markov Chain using Pyro
+def markov_model(data):
+    mu = pyro.sample('mu', dist.Normal(0, 10))
+    sigma = pyro.sample('sigma', dist.HalfNormal(10))
+    for i, value in enumerate(data):
+        pyro.sample(f"data_{i}", dist.Normal(mu, sigma), obs=value)
 
-    trace = pm.sample(1000, tune=1000, target_accept=0.95)
+def get_mcmc_samples(data, num_samples=1000, warmup_steps=200):
+    nuts_kernel = NUTS(markov_model)
+    mcmc = MCMC(nuts_kernel, num_samples=num_samples, warmup_steps=warmup_steps)
+    mcmc.run(torch.tensor(data))
+    return mcmc.get_samples()
 
-# 4. Posterior Predictive Checks for surrogate data
-ppc = pm.sample_posterior_predictive(trace, model=model, samples=1000)
-y_surrogate = np.mean(ppc['price_obs'], axis=0)
+def predict_next_value(data):
+    samples = get_mcmc_samples(data[:-1])
+    predicted_mu = samples['mu'].mean().item()
+    return predicted_mu
 
-# 5. Surrogate Modeling with Neural Network
+# 4. Create training data using MCMC predictions
+X = [seq[:9] for seq in sequences]
+y = [predict_next_value(seq) for seq in sequences]
+
+# 5. Surrogate modeling using Neural Network
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(32, activation='relu', input_shape=(9,)),
     tf.keras.layers.Dense(16, activation='relu'),
@@ -47,17 +59,22 @@ model = tf.keras.Sequential([
 ])
 
 model.compile(optimizer='adam', loss='mse')
-model.fit(X, y_surrogate, epochs=50, batch_size=32)
+model.fit(np.array(X), np.array(y), epochs=50, batch_size=32)
 
-from sklearn.metrics import mean_squared_error, mean_absolute_error
+# 6. Evaluate the model
+# Splitting data into train and test
+split_idx = int(0.8 * len(X))
+X_train, X_test = X[:split_idx], X[split_idx:]
+y_train, y_test = y[:split_idx], y[split_idx:]
 
-# Prediction using the surrogate model
-predictions = model.predict(X)
+# Make predictions on test set
+y_pred = model.predict(X_test)
 
-# Calculate Errors
-mse = mean_squared_error(y, predictions)
-mae = mean_absolute_error(y, predictions)
+# Calculate MSE and MAE
+mse = tf.keras.losses.MeanSquaredError()
+mae = tf.keras.losses.MeanAbsoluteError()
 
-print(f"Mean Squared Error: {mse}")
-print(f"Mean Absolute Error: {mae}")
+mse_value = mse(y_test, y_pred).numpy()
+mae_value = mae(y_test, y_pred).numpy()
 
+print(f"MSE: {mse_value}, MAE: {mae_value}")
